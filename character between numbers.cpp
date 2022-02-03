82f7ffddc5ca9ab691e20b57c2c261d554c79b70
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-02-03 13:02:20 +0330

    .

---
+++ character between numbers.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main ()
+{
+	// subtitle
+	printf ("This project is for symmetrical number. \n");
+	// variables
+	int incomingNum, stashNumMod, incomingNumReverse=0, doubleReverse=0;
+	char symbol;
+	
+	// incoming
+	printf ("Enter a number for enable format ");
+	scanf ("%d" , &incomingNum);
+	
+	printf ("Enter a symbol for drop between numbers ");
+	scanf ("%d" , &symbol);
+	
+	// variables
+	int incomingNumStash=incomingNum;
+	
+	// the operations	
+	while (incomingNum>0)
+	{
+		stashNumMod=incomingNum%10;
+		incomingNum=incomingNum/10;
+		incomingNumReverse=incomingNumReverse*10+stashNumMod;
+	}
+
+	
+	while (incomingNumReverse>0)
+	{
+		stashNumMod=incomingNumReverse%10;
+		incomingNumReverse=incomingNumReverse/10;
+	
+		// output	
+		printf ("%d" , stashNumMod);
+		printf ("%c" , symbol);
+	}
+	
+	
+	return 0;
+}
